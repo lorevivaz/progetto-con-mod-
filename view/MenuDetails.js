aeed089b2c975e870c093b3f9e55462682f4fec6
Lorenzo Vivarelli <lorenzo.vivarelli@studenti.unimi.it>
2024-11-29 17:02:21 +0100

    h

--- view/MenuDetails.js
+++ view/MenuDetails.js
@@ -20,50 +20,38 @@ const { location } = route.params; // qui prendiamo la location passata come par
 
     const handleBuyNow = async () => {
         try {
-
             // Recupera l'ultimo ordine salvato
             const lastOrder = await AsyncStorage.getItem('lastOrder');
-
+    
             if (lastOrder) {
-
                 const parsedOrder = JSON.parse(lastOrder);
     
                 // Controlla lo stato dell'ordine salvato
                 const sid = menu.sid;
-
                 const existingOrder = await fetchOrder(parsedOrder.oid, sid);
     
                 if (existingOrder && existingOrder.status !== 'COMPLETED') {
                     alert('Hai già un ordine attivo. Aspetta che l’ordine esistente arrivi.');
                     return;
-                } else if (existingOrder && existingOrder.status == 'COMPLETED') {
+                } else if (existingOrder && existingOrder.status === 'COMPLETED') {
                     // Cancella l'ordine esistente
-                   
                     await AsyncStorage.removeItem('lastOrder');
-                } 
-
+                }
             }
     
             // Effettua l'acquisto se non ci sono ordini attivi
-            const response = await fetchBuy(menu.mid , menu.sid,  location.latitude, location.longitude);
+            const response = await fetchBuy(menu.mid, menu.sid, location.latitude, location.longitude);
             console.log("Acquisto effettuato con successo:", response);
     
             // Salva il nuovo ordine
             await AsyncStorage.setItem('lastOrder', JSON.stringify({ oid: response.oid, mid: menu.mid }));
-
-
-            console.log("location :",  location)
-            // Naviga alla schermata Order.js con l'ordine appena creato e la positione dell'utente 
-
     
-
-            
-            navigation.navigate('Order' ); // qui passiamo il menuDetails alla schermata Order.js
-
-
+            console.log("Location:", location);
+    
+            // Naviga alla schermata Order.js
+            navigation.navigate('Order', { order: response });
+    
             alert('Acquisto completato con successo!');
-            
-    
         } catch (error) {
             console.error("Errore durante l'acquisto del menu:", error);
             alert('Errore durante l’acquisto. Riprova.');
